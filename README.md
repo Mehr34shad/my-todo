a71575e3430848af7846a168ba806f2f080d4da1
Mehr34shad <61622364+Mehr34shad@users.noreply.github.com>
2023-04-14 11:21:07 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ README.md
@@ -0,0 +1,6 @@
+<h2>Todo-list App</h2>
+
+<p>craete with ReactJs</p>
+
+
+![all](https://user-images.githubusercontent.com/61622364/209599035-a275166d-5cc2-409a-9ec5-9b2fbb981ae9.png)
---
+++ src/components/TodoForm.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { v4 as uuidv4 } from "uuid";
+
+function TodoForm(props) {
+  const [input, setInput] = useState(props.edit ? props.edit.value : '');
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+
+  const handleChange = e => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    props.onSubmit({
+      id: uuidv4(),
+      text: input
+    });
+    setInput('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className='todo-form'>
+      {props.edit ? (
+        <>
+          <input
+            placeholder='ویرایش کار'
+            value={input}
+            onChange={handleChange}
+            name='text'
+            ref={inputRef}
+            className='todo-input edit'
+          />
+          {/* <button onClick={handleSubmit} className='todo-button edit'>
+            ویرایش
+          </button> */}
+        </>
+      ) : (
+        <div id='inp'>
+          <input
+            placeholder='کار جدید'
+            value={input}
+            onChange={handleChange}
+            name='text'
+            className='todo-input'
+            ref={inputRef}
+          >
+            
+          </input>
+          {/* <button onClick={handleSubmit} className='todo-button'> */}
+            <i onClick={handleSubmit} className='todo-button' class="fa fa-plus" aria-hidden="true"></i>
+          {/* </button> */}
+        </div>
+      )}
+    </form>
+  );
+}
+
+export default TodoForm;
